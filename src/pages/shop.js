56c1027171c3ee8dd4951a47629aecf03d2b3018
cocoelizabeth <cocoelizabeth89@gmail.com>
2022-06-20 14:20:24 -0700

    testing gifs for shop

---
+++ src/pages/shop.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import logoGif from '../images/recess-chrome-v2-optimized.gif'
+import backgroundImage from '../images/shop/shop-background.jpg'
+import blackHoodie1 from '../images/shop/Black Hoodie 1.gif'
+import blackHoodie2 from '../images/shop/Black Hoodie 2.gif'
+import blackHoodie3 from '../images/shop/Black Hoodie 3.gif'
+import blackTee from '../images/shop/Black Tee.gif'
+import celeryHoodie from '../images/shop/Celery-Hoodie.gif'
+import fondantHoodie from '../images/shop/Fondant-Hoodie.gif'
+import naturalTee1 from '../images/shop/Natural-Tee-1.gif'
+import naturalTee2 from '../images/shop/Natural-Tee-2.gif'
+
+import '../css/font.css'
+import '../css/reset.css'
+import '../css/styles.css'
+import '../css/work-menu.css'
+import '../css/shop.css'
+
+
+const ShopPage = () => {
+    return (
+        <>
+            <div className="background-image-container">
+                <img className="shop-background-image" src={backgroundImage} alt="Background Image" />
+            </div>
+            <div className='content'>
+                <div className="product-container">
+                    <div className="product-image-container">
+                        <img className="product-image" src={blackHoodie1}></img>
+                    </div>
+                </div>
+                <div className="product-container">
+                    <div className="product-image-container">
+                        <img className="product-image" src={blackHoodie2}></img>
+                    </div>
+                </div>
+                <div className="product-container">
+                    <div className="product-image-container">
+                        <img className="product-image" src={blackHoodie3}></img>
+                    </div>
+                </div>
+                <div className="product-container">
+                    <div className="product-image-container">
+                        <img className="product-image" src={blackTee}></img>
+                    </div>
+                </div>
+                <div className="product-container">
+                    <div className="product-image-container">
+                        <img className="product-image" src={celeryHoodie}></img>
+                    </div>
+                </div>
+                <div className="product-container">
+                    <div className="product-image-container">
+                        <img className="product-image" src={fondantHoodie}></img>
+                    </div>
+                </div>
+                <div className="product-container">
+                    <div className="product-image-container">
+                        <img className="product-image" src={naturalTee1}></img>
+                    </div>
+                </div>
+                <div className="product-container">
+                    <div className="product-image-container">
+                        <img className="product-image" src={naturalTee2}></img>
+                    </div>
+                </div>
+            </div>
+        </>
+
+            
+
+    );
+}
+
+export default ShopPage
