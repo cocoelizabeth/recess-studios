67867b16f9fefd1d768912b778afe205b7b3a2b2
cocoelizabeth <cocoelizabeth89@gmail.com>
2022-07-21 10:22:44 -0400

    removed gatsby plugin commercelayer; will add back in for the ecomm feature push

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,8 +46,8 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp", 
     "gatsby-transformer-json",
-    `gatsby-plugin-netlify`,
-    `gatsby-plugin-commercelayer`
+    `gatsby-plugin-netlify`
+    // `gatsby-plugin-commercelayer`
 
     // "@contentful/rich-text-types"
   ]
