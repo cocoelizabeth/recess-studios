71a02a0ddca16dc8cf6c430881ae4a29f0304ec9
cocoelizabeth <cocoelizabeth89@gmail.com>
2022-07-26 14:46:36 -0400

    added placeholder for careers thankyou page

---
+++ src/pages/careers/thanks.js
@@ -0,0 +1,21 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+
+class CareersThanks extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+
+    render() {
+        return (
+            <h1>thanks</h1>
+        )
+    }
+
+}
+
+
+
+export default CareersThanks;
