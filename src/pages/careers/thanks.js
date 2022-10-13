089c1b32a795aac18861d2b7a8a96475ab2739d9
cocoelizabeth <cocoelizabeth89@gmail.com>
2022-10-13 10:35:15 -0400

    attempt to fix this.name is not defined bug by checking if props.location.state is null, and redirecting to careers if true

--- src/pages/careers/thanks.js
+++ src/pages/careers/thanks.js
@@ -6,25 +6,82 @@ import Header from "../../components/header";
 class CareersThanks extends React.Component {
     constructor(props) {
         super(props)
-        this.name = this.props.location.state.name || "";
-        this.location = this.props.location.state.selectedJobLocation || "";
-        this.jobTitle = this.props.location.state.selectedJobTitle || "";
+        if (this.props.location.state === null) {
+            this.state = {
+                name: "",
+                location: "",
+                jobTitle: ""
+            }
+        } else {
+            this.state = {
+                name: this.props.location.state.name,
+                location: this.props.location.state.selectedJobLocation,
+                jobTitle: this.props.location.state.selectedJobTitle
+            }
+        }
+        this.redirectRef = React.createRef();
+    }
+
+    componentDidMount() {
+        if (this.state.name === "") {
+            this.redirectRef.current.click();
+        }
+
     }
 
 
+    componentWillUnmount() {
+        
+    }
+
+
+
     render() {
-        return (
-            <div className="careers-container">
-                <Header leftText="Careers" background="black-header-background" leftTextLink="/careers/"/>
-               <div className="thanks-container">
-                    <h2 className="thank-you-h2">Thanks, {this.name}.</h2>
-                    <p className="thank-you-text">
-                        We have recieved your application for the {this.jobTitle} {" - "} {this.location} {" position."}
-                    </p>
-                    <Link className="underline-link" to="/careers/">RETURN TO CAREERS</Link>
-                </div>
-            </div>
-        )
+
+
+        
+            if (this.state.name != "") {
+                    let name = this.props.location.state.name;
+                    let location = this.props.location.state.selectedJobLocation;
+                    let jobTitle = this.props.location.state.selectedJobTitle;
+                    return (
+                        <div className="careers-container">
+                            <Header leftText="Careers" background="black-header-background" leftTextLink="/careers/"/>
+                            <div className="thanks-container">
+                                <h2 className="thank-you-h2">Thanks, {this.state.name}.</h2>
+                                <p className="thank-you-text">
+                                    We have recieved your application for the {this.state.jobTitle} {" - "} {this.state.location} {" position."}
+                                </p>
+                                <Link className="underline-link" to="/careers/">RETURN TO CAREERS</Link>
+                            </div>
+                        </div>
+                    )
+            } else {
+                return (
+                    <div className="careers-container">
+                        <Header leftText="Careers" background="black-header-background" leftTextLink="/careers/"/>
+                        <div className="thanks-container">
+                            {/* <h2 className="thank-you-h2">LOADING</h2>
+                            <p className="thank-you-text">
+                                We have recieved your application for the {this.jobTitle} {" - "} {this.location} {" position."}
+                            </p> */}
+                                <Link className="display-none" ref={this.redirectRef} className="underline-link" to="/careers/">RETURN TO CAREERS</Link>
+                        </div>
+                    </div>
+                )
+            }
+        // return (
+        //     <div className="careers-container">
+        //         <Header leftText="Careers" background="black-header-background" leftTextLink="/careers/"/>
+        //         <div className="thanks-container">
+        //             {/* <h2 className="thank-you-h2">Thanks, {this.name}.</h2>
+        //             <p className="thank-you-text">
+        //                 We have recieved your application for the {this.jobTitle} {" - "} {this.location} {" position."}
+        //             </p> */}
+        //             <Link className="underline-link" to="/careers/">RETURN TO CAREERS</Link>
+        //         </div>
+        //     </div>
+        // )
     }
 
 }
