29aadde410970982ce87374c6381ee831b6dfa1b
cocoelizabeth <cocoelizabeth89@gmail.com>
2022-10-14 15:29:40 -0400

    commented in career opportunites link

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ const IndexPage = () => {
           <a className="underline-link" href="mailto:321@recessworld.com" title="mailto:321@recessworld.com" target="_blank" rel="noreferrer noopener">321@RECESSWORLD.COM</a>
           <br></br>
           <br></br>
-          {/* <Link to="/careers" className="underline-link">CAREER OPPORTUNITIES</Link> */}
+          <Link to="/careers" className="underline-link">CAREER OPPORTUNITIES</Link>
         </div>
 
         <div className="grid-item grid-item-9 grid-item--width4 " />
